bc3e2a9e32a7e0823f2c8135ca4a8198e8f5fcf3
AhmedSafwat97 <xppahmad@gmail.com>
2023-12-31 09:21:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Restaurant_ConsoleApp__Project_using_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom to Our Resturant :) ");
+        }
+    }
+}

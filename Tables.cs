2a20815cebf186f223e95cfe30991a6e3b770110
Safi-Abu-Saad <alsafyabosaad3@gmail.com>
2024-01-02 20:04:33 +0200

    first prototype of Tables class

--- Tables.cs
+++ Tables.cs
@@ -6,15 +6,15 @@ public class Tables
     public int tableSize { get; set; }
     public bool reservedOrNot { get; set; }
 
-    public DateTime reservedFrom { get; set; }  
+    public DateTime? reservedFrom { get; set; }
 
-    public DateTime reservedTo { get; set; }
+    public DateTime? reservedTo { get; set; }
 
     private static List<Tables> sharedListOfReservedTables = new List<Tables>();
 
     public static List<Tables> reservedTables
     {
-        get { return reservedTables; }
+        get { return sharedListOfReservedTables; }
     }
 
     static Tables()
@@ -27,7 +27,30 @@ public class Tables
         tableID = ID;
         tableSize = size;
         reservedOrNot = false;
+        reservedFrom = null;
+        reservedTo = null;
     }
+
+    /*
+    public static List<Tables> generateData()
+    {
+        List<Tables> myTables = new List<Tables>();
+
+        for (int i = 1; i <= 10; i++)
+        {
+            myTables.Add(new Tables(i, 4));
+        }
+
+        for (int i = 11; i <= 20; i++)
+        {
+            myTables.Add(new Tables(i, 8));
+        }
+        
+        return myTables;
+    }
+    */
+
+
 
     public void reserveATable(Tables chosenTable)
     {
@@ -37,10 +60,19 @@ public class Tables
         }
         else
         {
-            Console.WriteLine("please enter when you want to reserve this table");
-            DateTime beginReservation = DateTime.TryParse(Console.ReadLine(), );
-            chosenTable.reservedOrNot = true;
-            sharedListOfReservedTables.Add(this);
+            //flag to keep track of reservation 
+            DateTime start;
+            DateTime end;
+            Console.WriteLine("please specify the time interval you want to reserve the table for: ");
+            if (DateTime.TryParse(Console.ReadLine(), out start) && DateTime.TryParse(Console.ReadLine(), out end))
+            {
+                chosenTable.reservedFrom = start;
+                chosenTable.reservedTo = end;
+                chosenTable.reservedOrNot = true;
+                sharedListOfReservedTables.Add(chosenTable);
+            }
+            else
+                Console.WriteLine("Please enter a valid date and time!");
         }
     }
 }
